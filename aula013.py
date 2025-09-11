65d660039274f6ad30df6f2b60c07a7fee784b8d
Luiz <jc381311@gmail.com>
2025-09-11 10:45:36 -0300

    tinder?

---
+++ aula013.py
@@ -0,0 +1,9 @@
+nome = 'Luiz Fernando'
+altura = 1.85
+peso = 75
+imc = 75 / (1.85 * 1.85)
+
+"f-strings"
+linha_1 = f' {nome} tem {altura} de altura e pesa {peso}kg.\n Seu IMC é dê: {imc:.2f}'
+
+print(linha_1)
