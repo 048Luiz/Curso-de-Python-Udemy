1ada661dde0acdaa57cb06bf4ae691ac9741fa25
Luiz <jc381311@gmail.com>
2025-09-12 09:29:57 -0300

    ela voltou

---
+++ aula015.py
@@ -0,0 +1,7 @@
+# nome = input('Qual o seu nome?') #retorna sempre string
+# print(f'Seu nome é {nome}!')
+
+numero_1 = input('Digite um número:')
+numero_2 = input('Digite outro número:')
+calc = int(numero_1) + int(numero_2)
+print(f'A soma dos dois números é dê: {calc}')
