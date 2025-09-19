749abf7708a84998a26f7bf2bce75f411279f27c
Luiz <jc381311@gmail.com>
2025-09-19 10:16:41 -0300

    aaaaaaaaa

---
+++ aula031.py
@@ -0,0 +1,20 @@
+"""
+Flago (Bandeira) - Marcar um local
+none = Não valor
+is e is not = é ou não é (tipo, valor, identidade)
+id = identidade
+"""
+
+v1 = 'a'
+v2 = 'b'
+print(id(v2))
+print(id(v2))
+
+# Condição = False
+
+
+# if condicao:
+#   print('Faça algo')
+
+# else: 
+#   print('Não faça algo')
